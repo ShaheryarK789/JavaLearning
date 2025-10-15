f9a8b85f7d9dbeadeb49264fe4288c7f4842035c
ShaheryarK789 <78638795+ShaheryarK789@users.noreply.github.com>
2025-10-15 13:07:00 +0500

    Update OverrideMethodChild.java

--- src/OopConcepts/OverrideMethodChild.java
+++ src/OopConcepts/OverrideMethodChild.java
@@ -1,26 +1,64 @@
 package OopConcepts;
 
-//To overcome any error on method over ridden we use @method Override annotation
+// The overriding method in the subclass must have the exact same number and types of parameters as the overridden method in the superclass. 
+//If the number or types of parameters differ, it is considered method overloading, not overriding.
 
-public class OverrideMethodChild extends OverrideMethodParent{
-	
-	@Override
-	public void car()  // Over-ridden Method
-	{
-		
-		System.out.println("Child Car Method");
-	}
-	
-	public void C()
-	{
-		System.out.println("Chile class method C");
-		
-		OverrideMethodParent p = new OverrideMethodParent();
-		p.car();
-		
-		OverrideMethodChild c = new OverrideMethodChild();
-		c.car();
-	}
-	
-	
+
+ class animal {
+
+    public void eat ()
+    {
+        System.out.println("This is eat method of Parent");
+    }
+
+    public void run(){
+        System.out.println("Run Method of Parent");
+    }
 }
+
+
+ class dog extends animal{
+
+    public void bark(){
+
+        System.out.println("This is the Child method bark");
+    }
+
+        public void run(){
+        System.out.println("Run Method of Child");
+        }
+
+}
+
+
+public class OverrideMethodChild{
+public static void main(String[] args)
+{
+    animal A = new animal();
+    dog    D = new    dog();
+    A.eat(); // Method of parent called
+    D.bark(); // dog's own method
+    D.eat(); // Method of parent called by child due to inheritence
+
+    //overloading
+    animal O = new dog();
+
+    O.run();   // Run Method of child will execute
+    
+/* you can create a reference variable of a parent (superclass) and assign it an object of its child (subclass).
+        This is called upcasting — a key part of runtime polymorphism.
+        ✅ Syntax:
+        Parent reference = new Child();    
+        
+        This way reusability - we just create only one object which is reference of parent and then call same method name of any sub classes at run time. 
+        only child class methods will run.
+        */
+
+} 
+
+
+}
+       
+      
+       
+  
