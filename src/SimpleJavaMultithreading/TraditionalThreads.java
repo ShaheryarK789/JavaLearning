f3c43c1c83267e8eb4687c714dd5f2747bb7fd08
ShaheryarK789 <78638795+ShaheryarK789@users.noreply.github.com>
2025-10-23 13:31:10 +0500

    Update and rename A.java to TraditionalThreads.java

---
+++ src/SimpleJavaMultithreading/TraditionalThreads.java
@@ -0,0 +1,42 @@
+/*Threads allows a program to operate more efficiently by doing multiple things at the same time.
+Threads can be used to perform complicated tasks in the background without interrupting the main program. 
+
+Extend classes from 'Thread' whose methods need to run in multithreading OR implements Runnable interface
+those methods of those class should be named as 'run' 
+Call the methods of those classes as 'start' like classobject.start  
+
+    1- Extend Thread ..............2-start .................. 3-run
+*/
+ class TraditionalThreads {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		B  ObjB = new B();
+		B ObjB1 = new B();
+		
+	
+		ObjB.start();
+		ObjB1.start();
+// Both threads will run in parallel
+	}
+}
+ class B  extends Thread 
+{
+	 public  void run ()
+	 {
+		 for (int  i=0; i<=10; i++)
+		 {
+			 
+			 System.out.println("Method--->B: "+ i); 
+			 try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+		 
+	 }
+	}
+
+ 
