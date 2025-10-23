13b8d0414395238bd0a2460e200f3d2ac21bafc1
ShaheryarK789 <78638795+ShaheryarK789@users.noreply.github.com>
2025-10-23 15:59:35 +0500

    Update Encapsulation.java

--- src/OopConcepts/Encapsulation.java
+++ src/OopConcepts/Encapsulation.java
@@ -24,16 +24,50 @@ Benefit of encapsulation
 how to use private data?
 -- using setters and getters method we can set and get the value of variable.
 -- private int num;
-*/
-public int getNum( )
-{
-return num;
-}
-
-public void setNum(int num)
-{
-this.num=num;
-}
 
  Note:
  Always make variable as private and method as public to achieve  security in program.
+
+*/
+class Human
+{
+	//int age;
+	//private int age=11;
+	private int age;
+	//String name;
+	//private String name="Navin";
+	private String name;
+	
+	public int getAge()
+	{
+		return age;
+	}
+	public void SetAge(int a)
+	{
+		age=a;
+	}
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String n)
+	{
+		name=n;
+	}
+}
+
+public class Demo {
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		Human obj=new Human();
+		obj.SetAge(30);
+		obj.setName("Reddy");
+//		obj.age=11;
+//		obj.name="Navin";
+		
+		System.out.println(obj.getName()+" : "+obj.getAge());
+	
+	}
+}
